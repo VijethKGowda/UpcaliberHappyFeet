74d941302b525f5e8c4b73cc6ac3c41420953556
vijeth k gowda <vijethkgowda@gmail.com>
2020-10-07 17:59:17 +0530

    button add to cart

--- src/components/product.js
+++ src/components/product.js
@@ -2,17 +2,16 @@ import React from "react";
 
 export const Product = ({ name, image, price, discount, addToCart }) => {
   return (
-    <div className="mx-4 w-full md:w-2/5 lg:w-1/4" key={name}>
-      <div className="flex flex-col flex-wrap flex-grow justify-between">
+    <div className="flex flex-col flex-wrap flex-grow justify-between mx-4 w-full md:w-2/5 lg:w-1/4" key={name}>
         <div className="p-4 w-full">
           <img alt="ecommerce" className="w-full h-48 my-4 block" src={image} />
-          <div className="flex flex-col justify-between items-center text-base">
+          <div className="flex flex-col flex-grow justify-between items-center h-auto text-base">
             <button
-              className="focus:outline-none float-right border-2 border-green-600 text-green-600 lg:text-sm text-xs font-bold py-2 text-center px-4"
+              className="focus:outline-none my-3 hover:bg-green-600 hover:text-white rounded-sm border-2 border-green-600 text-green-600 lg:text-sm text-xs font-bold py-1 text-center px-8"
               onClick={addToCart}
-            >buy</button>
-            <h2 className="text-gray-900 title-font text-lg font-medium">{name}</h2>
-            <div className="py-2">
+            >add to cart</button>
+            <h2 className="text-gray-900 title-font text-lg font-medium py-3">{name}</h2>
+            <div className="py-2 flex justify-start py-3">
               <span className="font-bold text-lg text-gray-700">
                 ₹{price.actual}
               </span>
@@ -23,10 +22,6 @@ export const Product = ({ name, image, price, discount, addToCart }) => {
             </div>
           </div>
         </div>
-        <div className="text-center py-2">
-
-        </div>
-      </div>
     </div>
   );
 }
